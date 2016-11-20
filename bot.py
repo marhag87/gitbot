a8ec7f07bf530686c471ba94e69ff36508927bca
Martin Hagstrom <marhag87@gmail.com>
2016-11-20 11:28:50 +0100

    Hook in main loop

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 from pprint import pprint
+import asyncio
 import discord
 from gitbot import (
+    events,
     new_events,
     parse_event,
 )
@@ -17,10 +19,12 @@ class BotError(Exception):
 
 _CONFIGFILE = 'config.yaml'
 _CLIENT = discord.Client()
+_SLEEPTIME = 5
 try:
     _CONFIG = load_config(_CONFIGFILE)
 except PyYAMLConfigError:
     _CONFIG = {}
+
 
 @_CLIENT.event
 async def on_ready():
@@ -82,8 +86,10 @@ def add_repo(full_repo, channel):
         repo = full_repo.split('/')[1]
     except IndexError:
         raise BotError('not a valid repo format')
+    if not isinstance(events(user, repo), dict):
+        raise BotError('it does not exist')
     repo_exists = False
-    for config_repo in _CONFIG.get('repos'):
+    for config_repo in _CONFIG.get('repos', []):
         if config_repo.get('user') == user and config_repo.get('repo') == repo:
             repo_exists = True
             if channel in config_repo.get('channels'):
@@ -92,6 +98,8 @@ def add_repo(full_repo, channel):
                 config_repo['channels'].append(channel)
 
     if not repo_exists:
+        if _CONFIG.get('repos') is None:
+            _CONFIG['repos'] = []
         _CONFIG['repos'].append(
             {
                 'user': user,
@@ -104,6 +112,11 @@ def add_repo(full_repo, channel):
         _CONFIGFILE,
         _CONFIG,
     )
+    reset_etag()
+
+
+def reset_etag():
+    pass
 
 
 def out(message):
@@ -111,22 +124,31 @@ def out(message):
     print('-------------------------')
 
 
-def main():
-    new_config = {
-        'token': _CONFIG.get('token'),
-        'clientid': _CONFIG.get('clientid'),
-        'repos': [],
-    }
-    for repo in _CONFIG.get('repos'):
-        (events, repo) = new_events(repo)
-        new_config['repos'].append(repo)
-        for event in events:
-            if event.get('type') in repo.get('events', []) or repo.get('events') is None:
-                out(parse_event(event))
+async def main_loop():
+    await _CLIENT.wait_until_ready()
+    while not _CLIENT.is_closed:
+        await asyncio.sleep(_SLEEPTIME)
+        new_config = {
+            'token': _CONFIG.get('token'),
+            'clientid': _CONFIG.get('clientid'),
+            'repos': [],
+        }
+        for repo in _CONFIG.get('repos', []):
+            (events, repo) = new_events(repo)
+            new_config['repos'].append(repo)
+            for event in events:
+                if event.get('type') in repo.get('events', []) or repo.get('events') is None:
+                    for channel in repo.get('channels'):
+                        message = parse_event(event)
+                        await _CLIENT.send_message(
+                            _CLIENT.get_channel(channel),
+                            message,
+                        )
 
-    #write_config(
-    #    _CONFIGFILE,
-    #    new_config,
-    #)
+        write_config(
+            _CONFIGFILE,
+            new_config,
+        )
 
+_CLIENT.loop.create_task(main_loop())
 _CLIENT.run(_CONFIG.get('token'))
